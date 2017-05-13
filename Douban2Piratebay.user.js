c8ca28f4a343b77ca62551baa9e40cbf14586054
bitdust <bitdust@users.noreply.github.com>
2017-05-13 15:08:33 +0800

    Update: update url

--- Douban2Piratebay.user.js
+++ Douban2Piratebay.user.js
@@ -5,8 +5,8 @@
 // @description  And direct link to piratebay from douban movie page.
 // @author       bitdust
 // @match        https://movie.douban.com/subject/*
-// @updateURL    https://github.com/bitdust/Douban2Piratebay/raw/master/Douban2Piratebay.meta.js
-// @downloadURL  https://github.com/bitdust/Douban2Piratebay/raw/master/Douban2Piratebay.user.js
+// @updateURL    https://raw.githubusercontent.com/bitdust/Douban2Piratebay/master/Douban2Piratebay.meta.js
+// @downloadURL  https://raw.githubusercontent.com/bitdust/Douban2Piratebay/master/Douban2Piratebay.user.js
 // @grant        none
 // ==/UserScript==
 
