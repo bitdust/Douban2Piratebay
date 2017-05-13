0807c41b01658896e954f69bbb046dbbb7b71c62
bitdust <bitdust16@gmail.com>
2017-05-13 14:30:44 +0800

    Update Douban2Piratebay.user.js

--- Douban2Piratebay.user.js
+++ Douban2Piratebay.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Douban2Piratebay
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/bitdust/Douban2Piratebay/
 // @version      0.1
 // @description  And direct link to piratebay from douban movie page.
 // @author       bitdust
 // @match        https://movie.douban.com/subject/*
+// @updateURL    https://github.com/bitdust/Douban2Piratebay/raw/master/Douban2Piratebay.meta.js
+// @downloadURL  https://github.com/bitdust/Douban2Piratebay/raw/master/Douban2Piratebay.user.js
 // @grant        none
 // ==/UserScript==
 
